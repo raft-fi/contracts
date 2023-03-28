0bf1cec870a4031f2950299d8af6bc7ba282aa78
Stefan Vitorovic <vitorovic.stefan@gmail.com>
2023-03-28 22:52:50 +0200

    refactor: Remove additional Liquity POS - v2 (#84)

--- test/TestContracts/Destructible.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.19;
-
-contract Destructible {
-    function destruct(address payable _receiver) external {
-        selfdestruct(_receiver);
-    }
-}
