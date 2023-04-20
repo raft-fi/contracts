a9420995af07a9ae5ce2c6d0215f8a363b451eb0
Djordje Mijovic <djordje.mijovic@gmail.com>
2023-04-20 23:17:54 +0200

    Implement one step leverage.
    Co-authored-by: Vukašin Manojlović <iamvukasin@gmail.com>

---
+++ contracts/Interfaces/IAMM.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/// @dev Interface that particular AMM integrations need to implement in order to be used in OneStepLeverage.
+/// Implementation will be used to swap between R and collateralToken.
+interface IAMM {
+    /// @dev Swaps `amountIn` of `tokenIn` for `tokenOut`. Fails if returned amount is smaller than `minReturn`.
+    /// @param tokenIn Address of the token that is being swapped.
+    /// @param tokenOut Address of the token to swap for.
+    /// @param amountIn Amount of `tokenIn` being swapped.
+    /// @param minReturn Minimum amount of `tokenOut` to get as a result of swap.
+    /// @param extraData Extra data for particular integration with DEX/Aggregator.
+    /// @return amountOut Actual amount that was returned from swap. Needs to be >= `minReturn`.
+    function swap(IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn, uint256 minReturn, bytes calldata extraData)
+        external
+        returns (uint256 amountOut);
+}
---
+++ test/TestContracts/MockAMM.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IAMM} from "../../contracts/Interfaces/IAMM.sol";
+
+/* Tester contract for math functions in Math.sol library. */
+
+error BelowMinReturn();
+
+contract MockAMM is IAMM {
+    IERC20 public immutable token1;
+    IERC20 public immutable token2;
+    uint256 public token1Rate; // expressed in token2
+
+    constructor(IERC20 _token1, IERC20 _token2, uint256 _token1Rate) {
+        token1 = _token1;
+        token2 = _token2;
+        token1Rate = _token1Rate;
+    }
+
+    function getAmountOut(IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn, bytes calldata)
+        private
+        view
+        returns (uint256 amountOut)
+    {
+        uint256 rate = _getRate(tokenIn, tokenOut);
+        return amountIn * rate / 1e18;
+    }
+
+    function swap(IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn, uint256 minReturn, bytes calldata extraData)
+        external
+        override
+        returns (uint256 amountReceived)
+    {
+        tokenIn.transferFrom(msg.sender, address(this), amountIn);
+        amountReceived = getAmountOut(tokenIn, tokenOut, amountIn, extraData);
+        if (amountReceived < minReturn) {
+            revert BelowMinReturn();
+        }
+        tokenOut.transfer(msg.sender, amountReceived);
+    }
+
+    function _getRate(IERC20 tokenIn, IERC20 tokenOut) internal view returns (uint256) {
+        if (tokenIn == token1 && tokenOut == token2) {
+            return token1Rate;
+        } else if (tokenIn == token2 && tokenOut == token1) {
+            return 1e18 * 1e18 / token1Rate;
+        }
+        revert("invalid tokens provided");
+    }
+}
