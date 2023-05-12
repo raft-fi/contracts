f5555e37a5f73c4f3378585f25034d23751623f5
yuval-weiss <43820038+yuval-weiss@users.noreply.github.com>
2023-05-12 15:34:13 +0200

    add reentrancyguard to flashloan (#326)

--- contracts/RToken.sol
+++ contracts/RToken.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import { IERC3156FlashBorrower } from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import { ERC20FlashMint, IERC3156FlashLender } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
@@ -9,7 +11,7 @@ import { PositionManagerDependent } from "./PositionManagerDependent.sol";
 import { IRToken } from "./Interfaces/IRToken.sol";
 import { FeeCollector } from "./FeeCollector.sol";
 
-contract RToken is ERC20Permit, ERC20FlashMint, PositionManagerDependent, FeeCollector, IRToken {
+contract RToken is ReentrancyGuard, ERC20Permit, ERC20FlashMint, PositionManagerDependent, FeeCollector, IRToken {
     // --- Constants ---
 
     uint256 public constant override PERCENTAGE_BASE = 10_000;
@@ -57,6 +59,20 @@ contract RToken is ERC20Permit, ERC20FlashMint, PositionManagerDependent, FeeCol
 
         flashMintFeePercentage = feePercentage;
         emit FlashMintFeePercentageChanged(flashMintFeePercentage);
+    }
+
+    function flashLoan(
+        IERC3156FlashBorrower receiver,
+        address token,
+        uint256 amount,
+        bytes calldata data
+    )
+        public
+        override(ERC20FlashMint, IERC3156FlashLender)
+        nonReentrant
+        returns (bool)
+    {
+        return super.flashLoan(receiver, token, amount, data);
     }
 
     /// @dev Inherited from ERC20FlashMint. Defines maximum size of the flash mint.
