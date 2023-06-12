024470890f9320b5fcfa83e6c00bd71ab91ffb4c
Djordje Mijovic <djordje.mijovic@gmail.com>
2023-06-12 14:21:03 +0200

    Give all the collateral to liquidator

--- contracts/SplitLiquidationCollateral.sol
+++ contracts/SplitLiquidationCollateral.sol
@@ -20,13 +20,7 @@ contract SplitLiquidationCollateral is ISplitLiquidationCollateral {
     uint256 private constant MEDIUM_REDISTRIBUTOR_REWARD_RATE = 125e14;
     uint256 private constant HIGH_REDISTRIBUTOR_REWARD_RATE = 50e14;
 
-    uint256 private constant LOW_LIQUIDATOR_REWARD_RATE = 1e18;
-    uint256 private constant MEDIUM_LIQUIDATOR_REWARD_RATE = 65e16;
-    uint256 private constant HIGH_LIQUIDATOR_REWARD_RATE = 50e16;
-
     uint256 public constant override LOW_TOTAL_DEBT = 3000e18;
-    uint256 private constant MEDIUM_TOTAL_DEBT = 100_000e18;
-    uint256 private constant HIGH_TOTAL_DEBT = 1_000_000e18;
 
     uint256 public constant override MCR = 110 * MathUtils._100_PERCENT / 100; // 110%
 
@@ -34,7 +28,7 @@ contract SplitLiquidationCollateral is ISplitLiquidationCollateral {
 
     function split(
         uint256 totalCollateral,
-        uint256 totalDebt,
+        uint256, /* totalDebt */
         uint256 price,
         bool isRedistribution
     )
@@ -47,11 +41,8 @@ contract SplitLiquidationCollateral is ISplitLiquidationCollateral {
             uint256 rewardRate = _calculateRedistributorRewardRate(collateralValue);
             collateralToSentToLiquidator = totalCollateral.mulDown(rewardRate);
         } else {
-            uint256 matchingCollateral = totalDebt.divDown(price);
-            uint256 excessCollateral = totalCollateral - matchingCollateral;
-            uint256 liquidatorReward = excessCollateral.mulDown(_calculateLiquidatorRewardRate(totalDebt));
-            collateralToSendToProtocol = excessCollateral - liquidatorReward;
-            collateralToSentToLiquidator = liquidatorReward + matchingCollateral;
+            collateralToSendToProtocol = 0;
+            collateralToSentToLiquidator = totalCollateral;
         }
     }
 
@@ -79,28 +70,6 @@ contract SplitLiquidationCollateral is ISplitLiquidationCollateral {
             );
         }
         return HIGH_REDISTRIBUTOR_REWARD_RATE;
-    }
-
-    // Formula from https://docs.raft.fi/how-it-works/returning/liquidation#liquidator-reward
-    function _calculateLiquidatorRewardRate(uint256 totalDebt) internal pure returns (uint256) {
-        if (totalDebt <= LOW_TOTAL_DEBT) {
-            return LOW_LIQUIDATOR_REWARD_RATE;
-        }
-        if (totalDebt <= MEDIUM_TOTAL_DEBT) {
-            return _calculateRewardRateFormula(
-                totalDebt, LOW_TOTAL_DEBT, MEDIUM_TOTAL_DEBT, LOW_LIQUIDATOR_REWARD_RATE, MEDIUM_LIQUIDATOR_REWARD_RATE
-            );
-        }
-        if (totalDebt <= HIGH_TOTAL_DEBT) {
-            return _calculateRewardRateFormula(
-                totalDebt,
-                MEDIUM_TOTAL_DEBT,
-                HIGH_TOTAL_DEBT,
-                MEDIUM_LIQUIDATOR_REWARD_RATE,
-                HIGH_LIQUIDATOR_REWARD_RATE
-            );
-        }
-        return HIGH_LIQUIDATOR_REWARD_RATE;
     }
 
     function _calculateRewardRateFormula(
