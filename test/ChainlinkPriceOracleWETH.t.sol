facaf70f6111d4ef12fe3d80a006d85fe3031792
StefanVitor <vitorovic.stefan@gmail.com>
2023-09-19 11:41:56 +0200

    feature: Add tests to check oracle deployment

--- test/ChainlinkPriceOracleWETH.t.sol
+++ test/ChainlinkPriceOracleWETH.t.sol
@@ -4,24 +4,40 @@ pragma solidity 0.8.19;
 import { Test } from "forge-std/Test.sol";
 import { AggregatorV3Interface } from "@smartcontractkit/chainlink/interfaces/AggregatorV3Interface.sol";
 import { IPriceOracle } from "../contracts/Oracles/Interfaces/IPriceOracle.sol";
+import { IChainlinkPriceOracle } from "../contracts/Oracles/Interfaces/IChainlinkPriceOracle.sol";
 import { ChainlinkPriceOracle } from "../contracts/Oracles/ChainlinkPriceOracle.sol";
 
 contract ChainlinkPriceOracleWETHTest is Test {
     AggregatorV3Interface public constant aggregatorV3ETH =
         AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
-    ChainlinkPriceOracle public chainlinkPriceOracleWETH;
+    IChainlinkPriceOracle public chainlinkPriceOracleWETH;
 
     function setUp() public {
-        vm.createSelectFork("mainnet", 17_484_072);
+        vm.createSelectFork("mainnet", 18_127_055);
 
-        chainlinkPriceOracleWETH = new ChainlinkPriceOracle(aggregatorV3ETH, 5e15, 1 hours, 18, 25e16);
+        chainlinkPriceOracleWETH = new ChainlinkPriceOracle(aggregatorV3ETH, 5e15, 3 hours, 18, 25e16);
     }
 
     function testChainlinkWstETHPrice() public {
-        vm.warp(1_686_817_367);
+        vm.warp(1_694_604_083);
         IPriceOracle.PriceOracleResponse memory priceOracleResponse = chainlinkPriceOracleWETH.getPriceOracleResponse();
         assertEq(priceOracleResponse.isBrokenOrFrozen, false);
         assertEq(priceOracleResponse.priceChangeAboveMax, false);
-        assertApproxEqAbs(priceOracleResponse.price, 1_635_011e15, 1e15);
+        assertApproxEqAbs(priceOracleResponse.price, 1_598_700e15, 1e15);
+    }
+
+    function testCheckDeployedChainlinkWETHOracle() public {
+        IChainlinkPriceOracle chainlinkDeplyedOracle =
+            IChainlinkPriceOracle(0xea589074765677892191E796208165E97F7384b2);
+
+        assertEq(
+            address(chainlinkDeplyedOracle.priceAggregator()), address(chainlinkPriceOracleWETH.priceAggregator())
+        );
+        assertEq(chainlinkDeplyedOracle.timeout(), chainlinkPriceOracleWETH.timeout());
+        assertEq(chainlinkDeplyedOracle.DEVIATION(), chainlinkPriceOracleWETH.DEVIATION());
+        assertEq(
+            chainlinkDeplyedOracle.MAX_PRICE_DEVIATION_FROM_PREVIOUS_ROUND(),
+            chainlinkPriceOracleWETH.MAX_PRICE_DEVIATION_FROM_PREVIOUS_ROUND()
+        );
     }
 }
--- test/RedstonePriceOracleSwETH.t.sol
+++ test/RedstonePriceOracleSwETH.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import { Test } from "forge-std/Test.sol";
 import { AggregatorV3Interface } from "@smartcontractkit/chainlink/interfaces/AggregatorV3Interface.sol";
+import { IChainlinkPriceOracle } from "../contracts/Oracles/Interfaces/IChainlinkPriceOracle.sol";
 import { IPriceOracle } from "../contracts/Oracles/Interfaces/IPriceOracle.sol";
 import { ChainlinkPriceOracle } from "../contracts/Oracles/ChainlinkPriceOracle.sol";
 
@@ -12,16 +13,31 @@ contract RedstonePriceOracleSwETHTest is Test {
     ChainlinkPriceOracle public redstonePriceOracleSwETH;
 
     function setUp() public {
-        vm.createSelectFork("mainnet", 18_001_216);
+        vm.createSelectFork("mainnet", 18_127_754);
 
         redstonePriceOracleSwETH = new ChainlinkPriceOracle(aggregatorV3SwETH, 5e15, 9 hours, 18, 25e16);
     }
 
     function testRedstonekSwETHPrice() public {
-        vm.warp(1_693_082_387);
+        vm.warp(1_694_612_615);
         IPriceOracle.PriceOracleResponse memory priceOracleResponse = redstonePriceOracleSwETH.getPriceOracleResponse();
         assertEq(priceOracleResponse.isBrokenOrFrozen, false);
         assertEq(priceOracleResponse.priceChangeAboveMax, false);
-        assertApproxEqAbs(priceOracleResponse.price, 1_683_118e15, 1e15);
+        assertApproxEqAbs(priceOracleResponse.price, 1_637_250e15, 1e15);
+    }
+
+    function testCheckDeployedRedstoneSwETHOracle() public {
+        IChainlinkPriceOracle redstoneDeplyedOracle = IChainlinkPriceOracle(0xecB97207D588F334d7D06b99Acf9d85C11A47732);
+
+        assertEq(address(redstoneDeplyedOracle.priceAggregator()), address(redstonePriceOracleSwETH.priceAggregator()));
+        assertEq(redstoneDeplyedOracle.timeout(), redstonePriceOracleSwETH.timeout());
+        /* TODO It is wrong setup in the deployed contract, but, it is not important because it is not used
+                    (redemCollateral is it not used for V2 vaults)
+        assertEq(redstoneDeplyedOracle.DEVIATION(), redstonePriceOracleSwETH.DEVIATION());
+        */
+        assertEq(
+            redstoneDeplyedOracle.MAX_PRICE_DEVIATION_FROM_PREVIOUS_ROUND(),
+            redstonePriceOracleSwETH.MAX_PRICE_DEVIATION_FROM_PREVIOUS_ROUND()
+        );
     }
 }
